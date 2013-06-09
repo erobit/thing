c31657e490b055a3545f1add3b1e80ee76de616f
Eric Robitaille <eric@vianet.ca>
2013-06-08 23:18:59 -0400

    oops. missing ;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var resource = require('resource'),
-    schemata = require('./schemata'),
-    thing = resource.define('thing');
+	schemata = require('./schemata'),
+	thing = resource.define('thing');
 
 thing.schema = schemata.minimal;
-thing.schema.description =  'a "big framework" resource for creating things - see thingtracker.net'
+thing.schema.description = 'a "big framework" resource for creating things - see thingtracker.net';
 exports.thing = thing;
