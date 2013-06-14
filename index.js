ebfb092b26b3bcc3b728110f9c4939f69435ce1e
Eric Robitaille <eric@vianet.ca>
2013-06-13 22:04:15 -0400

    reverted to static thing definition. Removed privat property for conformance to spec subset

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var resource = require('resource'),
-	schemata = require('./schemata'),
-	thing = resource.define('thing');
+		thing = resource.define('thing');
 
-thing.schema = schemata.minimal;
 thing.schema.description = 'thing - see thingtracket.net';
+
+thing.property('title': { type: "string", required: true });
+thing.property('url': { type: "string", required: true });
+thing.property('author': { type: "string" });
+thing.property('license': { type: "string" });
+thing.property('tags': { type: "array", items: { type: "string" }});
+thing.property('thumbnailURL': { type: "string" });
+thing.property('description': { type: "string" });
+
 exports.thing = thing;
