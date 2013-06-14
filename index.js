357d32b052f73337176b8301ae009f7900ed2715
Eric Robitaille <eric@vianet.ca>
2013-06-13 22:10:49 -0400

    fixed improper property definitions - single quotes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var resource = require('resource'),
 
 thing.schema.description = 'thing - see thingtracket.net';
 
-thing.property('title': { type: "string", required: true });
-thing.property('url': { type: "string", required: true });
-thing.property('author': { type: "string" });
-thing.property('license': { type: "string" });
-thing.property('tags': { type: "array", items: { type: "string" }});
-thing.property('thumbnailURL': { type: "string" });
-thing.property('description': { type: "string" });
+thing.property('title', { type: 'string', required: true });
+thing.property('url', { type: 'string', required: true });
+thing.property('author', { type: 'string' });
+thing.property('license', { type: 'string' });
+thing.property('tags', { type: 'array', items: { type: 'string' }});
+thing.property('thumbnailURL', { type: 'string' });
+thing.property('description', { type: 'string' });
 
 exports.thing = thing;
